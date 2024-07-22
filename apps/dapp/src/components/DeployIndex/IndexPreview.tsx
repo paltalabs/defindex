46dc54f66605ff337bf4401c9ea496ff1b696fc0
Matias Poblete <matias.pobleteduran@gmail.com>
2024-07-22 11:07:16 -0400

    ✨Add pieChart to deploy defindex preview

--- apps/dapp/src/components/DeployIndex/IndexPreview.tsx
+++ apps/dapp/src/components/DeployIndex/IndexPreview.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Table,
   Thead,
   Tbody,
@@ -9,34 +10,62 @@ import {
   Tooltip
 } from '@chakra-ui/react'
 import { shortenAddress } from '@/helpers/shortenAddress'
+import { PieChart, pieArcLabelClasses } from '@mui/x-charts'
 
 export const IndexPreview = ({ data }: { data: any }) => {
+  const myData = data.map((adapter: any, index: number) => {
+    return {
+      id: index,
+      label: adapter.name,
+      value: adapter.value
+    }
+  })
   return (
-    <TableContainer>
-      <Table variant={'simple'}>
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th textAlign={'center'}>Address</Th>
-            <Th textAlign={'end'}>Percentage</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.map((adapter: any, index: number) => (
-            <Tr key={index} sx={{ cursor: 'default' }}>
-              <Td>{adapter.name}</Td>
-
-              <Td sx={{ cursor: 'pointer' }} textAlign={'center'}>
-                <Tooltip label={adapter.address}>
-                  <p>{shortenAddress(adapter.address)}</p>
-                </Tooltip>
-              </Td>
-
-              <Td textAlign={'end'}>{adapter.value}%</Td>
+    <>
+      <Box display='flex' my={4}>
+        <PieChart
+          series={[
+            {
+              data: myData,
+            },
+          ]}
+          sx={{
+            [`& .${pieArcLabelClasses.root}`]: {
+              fill: 'white',
+              fontWeight: 'bold',
+            },
+          }}
+          tooltip={{ trigger: 'item' }}
+          width={500}
+          height={200}
+        />
+      </Box>
+      <TableContainer>
+        <Table variant={'simple'}>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th textAlign={'center'}>Address</Th>
+              <Th textAlign={'end'}>Percentage</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+          </Thead>
+          <Tbody>
+            {data.map((adapter: any, index: number) => (
+              <Tr key={index} sx={{ cursor: 'default' }}>
+                <Td>{adapter.name}</Td>
+
+                <Td sx={{ cursor: 'pointer' }} textAlign={'center'}>
+                  <Tooltip label={adapter.address}>
+                    <p>{shortenAddress(adapter.address)}</p>
+                  </Tooltip>
+                </Td>
+
+                <Td textAlign={'end'}>{adapter.value}%</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
   )
 }
--- apps/dapp/src/providers/main-provider.tsx
+++ apps/dapp/src/providers/main-provider.tsx
@@ -1,15 +1,26 @@
+'use client'
 import React from 'react'
 import { StoreProvider } from './store-provider'
 import MySorobanReactProvider from './soroban-react-provider'
 import { ThemeProvider } from './chakra-provider'
+import { createTheme, ThemeProvider as MuiProvider } from '@mui/material/styles';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const theme = createTheme(
+    {
+      palette: {
+        primary: { main: '#1976d2' },
+      },
+    },
+  );
   return (
     <StoreProvider>
       <MySorobanReactProvider>
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+        <MuiProvider theme={theme}>
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </MuiProvider>
       </MySorobanReactProvider>
     </StoreProvider>
   )
