ff8fcd54117acd70fe72a759f6f342fc41eca5ee
coderipper <hello@joaquinsoza.dev>
2024-09-03 14:56:35 -0400

    added token for defindex vault

--- apps/contracts/defindex/src/lib.rs
+++ apps/contracts/defindex/src/lib.rs
@@ -3,7 +3,11 @@ use soroban_sdk::{
     auth::{ContractContext, InvokerContractAuthEntry, SubContractInvocation},
     contract, contractimpl, vec, Address, Env, IntoVal, String, Symbol, Val, Vec,
 };
+use soroban_token_sdk::metadata::TokenMetadata;
 
+
+
+mod token;
 mod error;
 mod models;
 mod storage;
@@ -16,7 +20,8 @@ use storage::{
 };
 
 use models::AdapterParams;
-use defindex_adapter_interface::{DeFindexAdapterClient};
+use defindex_adapter_interface::DeFindexAdapterClient;
+use token::write_metadata;
 
 fn check_initialized(e: &Env) -> Result<(), ContractError> {
     if is_initialized(e) {
@@ -33,7 +38,7 @@ pub trait AllocatorTrait {
 
     fn get_adapter_address(e: Env) -> Address;
 
-    fn balance(
+    fn shares(
         e: Env,
         from: Address,
     ) -> Result<Vec<i128>, ContractError>;
@@ -55,13 +60,25 @@ impl AllocatorTrait for Allocator {
         }
 
         // should verify that shares are not more than 100%
+        let decimal: u32 = 7;
+        let name: String = String::from_str(&e, "dfToken");
+        let symbol: String = String::from_str(&e, "DFT");
+    
+        write_metadata(
+            &e,
+            TokenMetadata {
+                decimal ,
+                name,
+                symbol,
+            },
+        );
 
         set_initialized(&e, true);
         set_total_adapters(&e, &adapters.len());
 
         for adapter in adapters.iter() {
-            set_share(&e, adapter.index.clone(), adapter.share.clone());
-            set_adapter(&e, adapter.index.clone(), &adapter.address);
+            set_share(&e, adapter.index, adapter.share);
+            set_adapter(&e, adapter.index, &adapter.address);
         }
 
         Ok(())
@@ -72,7 +89,7 @@ impl AllocatorTrait for Allocator {
         from.require_auth();
 
         let total_adapters = get_total_adapters(&e);
-        let mut total_amount_used: i128 = 0;
+        let total_amount_used: i128 = 0;
 
         for i in 0..total_adapters {
             let adapter_share = get_share(&e, i);
@@ -89,7 +106,7 @@ impl AllocatorTrait for Allocator {
                     .ok_or(ContractError::ArithmeticError)?
             };
 
-            let response = adapter_client.deposit(&adapter_amount, &from);
+            adapter_client.deposit(&adapter_amount, &from);
             //should run deposit functions on adapters
         }
 
@@ -100,7 +117,7 @@ impl AllocatorTrait for Allocator {
         get_adapter(&e, 0)
     }
 
-    fn balance(
+    fn shares(
         e: Env,
         from: Address,
     ) -> Result<Vec<i128>, ContractError> {
