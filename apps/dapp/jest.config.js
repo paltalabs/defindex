52f2fe49d77f855bac1780a717de6fa961b4946a
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-10-01 20:19:57 -0300

    ♻️ Refactor jest config

--- apps/dapp/jest.config.js
+++ apps/dapp/jest.config.js
@@ -1,5 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 const nextJest = require("next/jest");
-
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
