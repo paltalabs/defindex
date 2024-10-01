c2c42d358aebe6bd3a9e62437bd17e034dc4ab1b
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-10-01 19:29:12 -0300

    👷 Add dependencies & configure jest to run tests

--- apps/dapp/jest.config.js
+++ apps/dapp/jest.config.js
@@ -9,6 +9,9 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  }
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
