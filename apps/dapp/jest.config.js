4aa75d640b30c60569e88b1069eebf58df88dcc0
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-10-01 20:11:03 -0300

    ➕ replaced babel with ts-jest

--- apps/dapp/jest.config.js
+++ apps/dapp/jest.config.js
@@ -10,7 +10,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
   transform: {
-    '^.+\\.tsx?$': 'babel-jest',
+    "^.+.tsx?$": ["ts-jest",{}],
   }
 };
 
