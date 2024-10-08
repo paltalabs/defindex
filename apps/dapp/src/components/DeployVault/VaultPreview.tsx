eff1c1701ccfbc9b2b184cf3c576b7efa1a35ebf
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-10-08 12:41:51 +0100

    ✨Add address validation to vault managers & feeReciever

--- apps/dapp/src/components/DeployVault/VaultPreview.tsx
+++ apps/dapp/src/components/DeployVault/VaultPreview.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Box,
   Table,
@@ -18,23 +18,102 @@ import { shortenAddress } from '@/helpers/shortenAddress'
 import { PieChart } from '@mui/x-charts'
 import { ChartData } from './ConfirmDelpoyModal'
 import { setEmergencyManager, setFeeReceiver, setManager } from '@/store/lib/features/vaultStore'
-import { useAppDispatch, useAppSelector } from '@/store/lib/storeHooks'
+import { useAppDispatch } from '@/store/lib/storeHooks'
+import { StrKey } from '@stellar/stellar-sdk'
 
 
+interface FormControlInterface {
+  manager: {
+    isValid: boolean | undefined;
+  },
+  emergencyManager: {
+    isValid: boolean | undefined;
+  },
+  feeReceiver: {
+    isValid: boolean | undefined;
+  },
+}
 export const VaultPreview = ({ data }: { data: ChartData[] }) => {
+
   const dispatch = useAppDispatch()
+  const [formControl, setFormControl] = useState<FormControlInterface>({
+    manager: {
+      isValid: undefined,
+    },
+    emergencyManager: {
+      isValid: undefined,
+    },
+    feeReceiver: {
+      isValid: undefined,
+    },
+  })
+  const isValidAddress = (address: string) => {
+    if (StrKey.isValidEd25519PublicKey(address) || StrKey.isValidMed25519PublicKey(address) || StrKey.isValidContract(address)) {
+      return true
+    } else {
+      return false
+    }
+  }
   const handleManagerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isValid = isValidAddress(event.target.value)
+    while (!isValid) {
+      setFormControl({
+        ...formControl,
+        manager: {
+          isValid: false,
+        }
+      })
+      dispatch(setManager(''))
+      return
+    }
+    setFormControl({
+      ...formControl,
+      manager: {
+        isValid: true,
+      }
+    })
     dispatch(setManager(event.target.value))
-    // Your logic here using the address
   };
 
   const handleEmergencyManagerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isValid = isValidAddress(event.target.value)
+    while (!isValid) {
+      setFormControl({
+        ...formControl,
+        emergencyManager: {
+          isValid: false,
+        }
+      })
+      dispatch(setEmergencyManager(''))
+      return
+    }
+    setFormControl({
+      ...formControl,
+      emergencyManager: {
+        isValid: true,
+      }
+    })
     dispatch(setEmergencyManager(event.target.value))
-    // Your logic here using the address
   };
   const handleFeeReceiverChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isValid = isValidAddress(event.target.value)
+    while (!isValid) {
+      setFormControl({
+        ...formControl,
+        feeReceiver: {
+          isValid: false,
+        }
+      })
+      dispatch(setFeeReceiver(''))
+      return
+    }
+    setFormControl({
+      ...formControl,
+      feeReceiver: {
+        isValid: true,
+      }
+    })
     dispatch(setFeeReceiver(event.target.value))
-    // Your logic here using the address
   };
 
   return (
@@ -89,19 +168,34 @@ export const VaultPreview = ({ data }: { data: ChartData[] }) => {
           <Text mt={4}>Manager</Text>
         </GridItem>
         <GridItem colStart={5} colSpan={3} rowStart={1}>
-          <Input onChange={handleManagerChange} placeholder='GAFS3TLVM...'></Input>
+          <Input
+            onChange={handleManagerChange}
+            placeholder='GAFS3TLVM...'
+            isRequired
+            isInvalid={formControl.manager.isValid === false}
+          />
         </GridItem>
         <GridItem colStart={2} colSpan={3} rowStart={3}>
           <Text mt={4}>Emergency Manager</Text>
         </GridItem>
         <GridItem colStart={5} colSpan={3} rowStart={3}>
-          <Input onChange={handleEmergencyManagerChange} placeholder='GAFS3TLVM...'></Input>
+          <Input
+            onChange={handleEmergencyManagerChange}
+            placeholder='GAFS3TLVM...'
+            isRequired
+            isInvalid={formControl.emergencyManager.isValid === false}
+          />
         </GridItem>
         <GridItem colStart={2} colSpan={3} rowStart={5}>
           <Text mt={4}>Fee Receiver</Text>
         </GridItem>
         <GridItem colStart={5} colSpan={3} rowStart={5}>
-          <Input onChange={handleFeeReceiverChange} placeholder='GAFS3TLVM...'></Input>
+          <Input
+            onChange={handleFeeReceiverChange}
+            placeholder='GAFS3TLVM...'
+            isRequired
+            isInvalid={formControl.feeReceiver.isValid === false}
+          />
         </GridItem>
       </Grid>
     </>
