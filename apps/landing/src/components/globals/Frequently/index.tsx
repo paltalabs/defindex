7fe41465a203d23ebd6ff593c910c703353293fe
chopan123 <chopancatrileo@gmail.com>
2024-10-15 08:18:58 +0100

    FAQs

--- apps/landing/src/components/globals/Frequently/index.tsx
+++ apps/landing/src/components/globals/Frequently/index.tsx
@@ -11,26 +11,22 @@ function Frequently() {
                             Frequently Asked Questions
                         </h2>
                         <p className="font-inter-tight text-[20px] xl:text-lg leading-[1.25em] text-white">
-                            Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi
-                            ut aliquip ex ea commodo consequat aute irure.
+                            DeFindex makes it easy for wallet providers to offer yield-generating accounts to their users through diverse DeFi strategies. It provides seamless integration, security, and transparency, ensuring both developers and users can benefit from passive income and innovative financial tools.
                         </p>
                     </div>
                     <div className="flex flex-col gap-6">
                         <Unit
                             isOpen
-                            title="Frequently Asked Questions 1"
-                            description="Ut enim ad minim veniam quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat aute irure dolor"
+                            title="What is DeFindex, and how does it work? "
+                            description="DeFindex allows wallet providers to integrate automated, secure, and diversified DeFi strategies into their applications, enabling users to earn passive income from cryptocurrencies. It operates through smart contracts, which handle everything from reinvestment to securing funds."
                         />
                         <Unit
-                            title="Frequently Asked Questions 2"
-                            description="Ut enim ad minim veniam quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat aute irure dolor"
+                            title="How can DeFindex benefit wallet builders?"
+                            description="Wallet builders can customize portfolios using a variety of DeFi strategies, enhancing user engagement with yield-generating accounts. DeFindex provides easy integration tools that allow users to start earning with just one click."
                         />{" "}
                         <Unit
-                            title="Frequently Asked Questions 3"
-                            description="Ut enim ad minim veniam quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat aute irure dolor"
+                            title="Is DeFindex secure and decentralized?"
+                            description="Yes, DeFindex operates through secure and transparent smart contracts, ensuring that all transactions are decentralized and under the full control of the user, providing peace of mind to partners and users alike."
                         />
                     </div>
                 </div>
