f086e15a018f17495f8754dae1d2065343685b22
chopan123 <chopancatrileo@gmail.com>
2024-10-30 15:42:17 -0300

    refactor,

--- apps/contracts/src/tests/dev.ts
+++ apps/contracts/src/tests/dev.ts
@@ -4,7 +4,8 @@ import { Keypair } from "@stellar/stellar-sdk";
 
 const user = Keypair.fromSecret("SA77N6PLHDFRYDNYE3YJQBPTRNODMVYP5WWF2SG42DXB52GW2FWOG2B3")
 const contract = "CCNWF3D7FJCZKYCAD6FAO3JNPRHG6SVXHO5YTFDZRXSPOJXL6TIBWP3Y"
+await withdrawFromVault(contract, BigInt(10000), user)
 
-const badUser = Keypair.random()
-await airdropAccount(badUser);
-await withdrawFromVault(contract, BigInt(100000000), badUser)
+// const badUser = Keypair.random()
+// await airdropAccount(badUser);
+// await withdrawFromVault(contract, BigInt(1000), badUser)
