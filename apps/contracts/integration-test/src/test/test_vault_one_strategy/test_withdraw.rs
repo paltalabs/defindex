9bc1d949483f9bdf5a400bf4f81ba311fb08e8d6
coderipper <hello@joaquinsoza.dev>
2024-11-14 12:42:55 -0300

    withdraw + invest + multiple users

--- apps/contracts/integration-test/src/test/test_vault_one_strategy/test_withdraw.rs
+++ apps/contracts/integration-test/src/test/test_vault_one_strategy/test_withdraw.rs
@@ -383,3 +383,346 @@ fn test_withdraw_after_invest() {
     let vault_balance_after_withdraw = enviroment.token.balance(&enviroment.vault_contract.address);
     assert_eq!(vault_balance_after_withdraw, 0);
 }
+
+#[test]
+fn test_withdraw_multiple_users() {
+    let enviroment = create_vault_one_asset_hodl_strategy();
+    let setup = enviroment.setup;
+
+    let user_starting_balance = 100_000_0_000_000i128;
+
+    let users = IntegrationTest::generate_random_users(&setup.env, 2);
+    let user1 = &users[0];
+    let user2 = &users[1];
+
+    enviroment.token_admin_client.mock_auths(&[MockAuth {
+        address: &enviroment.token_admin.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.token.address.clone(),
+            fn_name: "mint",
+            args: (user1, user_starting_balance,).into_val(&setup.env),
+            sub_invokes: &[],
+        },
+    }]).mint(user1, &user_starting_balance);
+    enviroment.token_admin_client.mock_auths(&[MockAuth {
+        address: &enviroment.token_admin.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.token.address.clone(),
+            fn_name: "mint",
+            args: (user2, user_starting_balance,).into_val(&setup.env),
+            sub_invokes: &[],
+        },
+    }]).mint(user2, &user_starting_balance);
+
+    let user1_balance = enviroment.token.balance(user1);
+    let user2_balance = enviroment.token.balance(user2);
+    assert_eq!(user1_balance, user_starting_balance);
+    assert_eq!(user2_balance, user_starting_balance);
+
+    let deposit_amount = 10_000_0_000_000i128;
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &user1.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "deposit",
+            args: (
+                Vec::from_array(&setup.env,[deposit_amount]),
+                Vec::from_array(&setup.env,[deposit_amount]),
+                user1.clone()
+            ).into_val(&setup.env),
+            sub_invokes: &[
+                MockAuthInvoke {
+                    contract: &enviroment.token.address.clone(),
+                    fn_name: "transfer",
+                    args: (
+                        user1.clone(), 
+                        &enviroment.vault_contract.address.clone(),
+                        deposit_amount
+                     ).into_val(&setup.env),
+                    sub_invokes: &[]
+                }
+            ]
+        },
+    }])
+    .deposit(&svec![&setup.env, deposit_amount], &svec![&setup.env, deposit_amount], &user1);
+
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &user2.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "deposit",
+            args: (
+                Vec::from_array(&setup.env,[deposit_amount]),
+                Vec::from_array(&setup.env,[deposit_amount]),
+                user2.clone()
+            ).into_val(&setup.env),
+            sub_invokes: &[
+                MockAuthInvoke {
+                    contract: &enviroment.token.address.clone(),
+                    fn_name: "transfer",
+                    args: (
+                        user2.clone(), 
+                        &enviroment.vault_contract.address.clone(),
+                        deposit_amount
+                     ).into_val(&setup.env),
+                    sub_invokes: &[]
+                }
+            ]
+        },
+    }])
+    .deposit(&svec![&setup.env, deposit_amount], &svec![&setup.env, deposit_amount], &user2);
+
+    let df_balance_user1 = enviroment.vault_contract.balance(&user1);
+    let df_balance_user2 = enviroment.vault_contract.balance(&user2);
+    assert_eq!(df_balance_user1, deposit_amount - MINIMUM_LIQUIDITY);
+    assert_eq!(df_balance_user2, deposit_amount);
+
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &user1.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "withdraw",
+            args: (
+                df_balance_user1.clone(),
+                user1.clone()
+            ).into_val(&setup.env),
+            sub_invokes: &[]
+        },
+    }])
+    .withdraw(&df_balance_user1, &user1);
+
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &user2.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "withdraw",
+            args: (
+                df_balance_user2.clone(),
+                user2.clone()
+            ).into_val(&setup.env),
+            sub_invokes: &[]
+        },
+    }])
+    .withdraw(&df_balance_user2, &user2);
+
+    let df_balance_user1 = enviroment.vault_contract.balance(&user1);
+    let df_balance_user2 = enviroment.vault_contract.balance(&user2);
+    assert_eq!(df_balance_user1, 0);
+    assert_eq!(df_balance_user2, 0);
+
+    let vault_balance_after_withdraw = enviroment.token.balance(&enviroment.vault_contract.address);
+    assert_eq!(vault_balance_after_withdraw, MINIMUM_LIQUIDITY);
+
+    let user1_balance_after_withdraw = enviroment.token.balance(user1);
+    let user2_balance_after_withdraw = enviroment.token.balance(user2);
+    assert_eq!(user1_balance_after_withdraw, user_starting_balance - MINIMUM_LIQUIDITY);
+    assert_eq!(user2_balance_after_withdraw, user_starting_balance);
+
+    let total_supply = enviroment.vault_contract.total_supply();
+    assert_eq!(total_supply, MINIMUM_LIQUIDITY);
+}
+
+#[test]
+fn test_withdraw_after_invest_multiple_users() {
+    let enviroment = create_vault_one_asset_hodl_strategy();
+    let setup = enviroment.setup;
+
+    let user_starting_balance = 10_000_0_000_000i128;
+
+    let users = IntegrationTest::generate_random_users(&setup.env, 2);
+    let user1 = &users[0];
+    let user2 = &users[1];
+
+    enviroment.token_admin_client.mock_auths(&[MockAuth {
+        address: &enviroment.token_admin.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.token.address.clone(),
+            fn_name: "mint",
+            args: (user1, user_starting_balance,).into_val(&setup.env),
+            sub_invokes: &[],
+        },
+    }]).mint(user1, &user_starting_balance);
+    enviroment.token_admin_client.mock_auths(&[MockAuth {
+        address: &enviroment.token_admin.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.token.address.clone(),
+            fn_name: "mint",
+            args: (user2, user_starting_balance,).into_val(&setup.env),
+            sub_invokes: &[],
+        },
+    }]).mint(user2, &user_starting_balance);
+
+    let user1_balance = enviroment.token.balance(user1);
+    let user2_balance = enviroment.token.balance(user2);
+    assert_eq!(user1_balance, user_starting_balance);
+    assert_eq!(user2_balance, user_starting_balance);
+
+    let deposit_amount = 10_000_0_000_000i128;
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &user1.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "deposit",
+            args: (
+                Vec::from_array(&setup.env,[deposit_amount]),
+                Vec::from_array(&setup.env,[deposit_amount]),
+                user1.clone()
+            ).into_val(&setup.env),
+            sub_invokes: &[
+                MockAuthInvoke {
+                    contract: &enviroment.token.address.clone(),
+                    fn_name: "transfer",
+                    args: (
+                        user1.clone(), 
+                        &enviroment.vault_contract.address.clone(),
+                        deposit_amount
+                     ).into_val(&setup.env),
+                    sub_invokes: &[]
+                }
+            ]
+        },
+    }])
+    .deposit(&svec![&setup.env, deposit_amount], &svec![&setup.env, deposit_amount], &user1);
+
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &user2.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "deposit",
+            args: (
+                Vec::from_array(&setup.env,[deposit_amount]),
+                Vec::from_array(&setup.env,[deposit_amount]),
+                user2.clone()
+            ).into_val(&setup.env),
+            sub_invokes: &[
+                MockAuthInvoke {
+                    contract: &enviroment.token.address.clone(),
+                    fn_name: "transfer",
+                    args: (
+                        user2.clone(), 
+                        &enviroment.vault_contract.address.clone(),
+                        deposit_amount
+                     ).into_val(&setup.env),
+                    sub_invokes: &[]
+                }
+            ]
+        },
+    }])
+    .deposit(&svec![&setup.env, deposit_amount], &svec![&setup.env, deposit_amount], &user2);
+
+    let df_balance_user1 = enviroment.vault_contract.balance(&user1);
+    let df_balance_user2 = enviroment.vault_contract.balance(&user2);
+    assert_eq!(df_balance_user1, deposit_amount - MINIMUM_LIQUIDITY);
+    assert_eq!(df_balance_user2, deposit_amount);
+
+    // Create investment strategies for the deposited tokens
+    let investments = svec![
+        &setup.env,
+        Some(AssetInvestmentAllocation {
+            asset: enviroment.token.address.clone(),
+            strategy_investments: svec![
+                &setup.env,
+                Some(StrategyInvestment {
+                    strategy: enviroment.strategy_contract.address.clone(),
+                    amount: deposit_amount * 2,
+                }),
+            ],
+        }),
+    ];
+
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &enviroment.manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "invest",
+            args: (
+                Vec::from_array(&setup.env,[
+                    Some(
+                        AssetInvestmentAllocation {
+                            asset: enviroment.token.address.clone(),
+                            strategy_investments:
+                                svec![&setup.env,
+                                    Some(StrategyInvestment {
+                                        strategy: enviroment.strategy_contract.address.clone(),
+                                        amount: deposit_amount * 2,
+                                    })
+                                ]
+                        }
+                    )
+                ]),
+            ).into_val(&setup.env),
+            sub_invokes: &[]
+        },
+    }])
+    .invest(&investments);
+
+    setup.env.ledger().set_timestamp(setup.env.ledger().timestamp() + ONE_YEAR_IN_SECONDS);
+
+    let token_balance_after_invest = enviroment.token.balance(&enviroment.vault_contract.address);
+    assert_eq!(token_balance_after_invest, 0);
+
+    let strategy_balance = enviroment.strategy_contract.balance(&enviroment.vault_contract.address);
+    assert_eq!(strategy_balance, deposit_amount * 2);
+    
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &user1.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "withdraw",
+            args: (
+                df_balance_user1.clone(),
+                user1.clone()
+            ).into_val(&setup.env),
+            sub_invokes: &[]
+        },
+    }])
+    .withdraw(&df_balance_user1, &user1);
+
+    enviroment.vault_contract
+    .mock_auths(&[MockAuth {
+        address: &user2.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &enviroment.vault_contract.address.clone(),
+            fn_name: "withdraw",
+            args: (
+                df_balance_user2.clone(),
+                user2.clone()
+            ).into_val(&setup.env),
+            sub_invokes: &[]
+        },
+    }])
+    .withdraw(&df_balance_user2, &user2);
+
+    let df_balance_user1 = enviroment.vault_contract.balance(&user1);
+    let df_balance_user2 = enviroment.vault_contract.balance(&user2);
+    assert_eq!(df_balance_user1, 0);
+    assert_eq!(df_balance_user2, 0);
+
+    let token_balance_after_withdraw = enviroment.token.balance(&enviroment.vault_contract.address);
+    assert_eq!(token_balance_after_withdraw, 0);
+
+    let charged_fee_user1 = (deposit_amount - MINIMUM_LIQUIDITY) * (DEFINDEX_FEE as i128 + VAULT_FEE as i128) / 10000;
+    let expected_amount_user1 = deposit_amount - MINIMUM_LIQUIDITY - charged_fee_user1;
+
+    let charged_fee_user2 = deposit_amount * (DEFINDEX_FEE as i128 + VAULT_FEE as i128) / 10000;
+    let expected_amount_user2 = deposit_amount - charged_fee_user2;
+
+    let user1_balance_after_withdraw = enviroment.token.balance(user1);
+    let user2_balance_after_withdraw = enviroment.token.balance(user2);
+    assert_eq!(user1_balance_after_withdraw, expected_amount_user1);
+    assert_eq!(user2_balance_after_withdraw, expected_amount_user2);
+
+    let strategy_balance = enviroment.strategy_contract.balance(&enviroment.vault_contract.address);
+    assert_eq!(strategy_balance, charged_fee_user1 + charged_fee_user2 + MINIMUM_LIQUIDITY);   
+
+    let vault_balance_after_withdraw = enviroment.token.balance(&enviroment.vault_contract.address);
+    assert_eq!(vault_balance_after_withdraw, 0);
+}
