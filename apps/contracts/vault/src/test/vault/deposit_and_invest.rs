0354a753a5ba01462b72b935e18040378b6bf053
chopan123 <chopancatrileo@gmail.com>
2024-12-12 09:32:39 -0300

    hotfix: removed warning

--- apps/contracts/vault/src/test/vault/deposit_and_invest.rs
+++ apps/contracts/vault/src/test/vault/deposit_and_invest.rs
@@ -584,7 +584,7 @@ fn several_assets_wih_previous_investment_success() {
     test.token1_admin_client.mint(&users[0], &amount1);
    
     // deposit with no previous investment
-    let deposit_result=defindex_contract.deposit(
+    defindex_contract.deposit(
         &sorobanvec![&test.env, amount0, amount1],
         &sorobanvec![&test.env, amount0, amount1],
         &users[0],
