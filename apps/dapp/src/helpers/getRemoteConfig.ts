7f1708459e260a8da815d1d558a4bc4ede488ec5
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-12-16 18:09:21 -0300

    ♻️Refactor GetRemoteConfig

--- apps/dapp/src/helpers/getRemoteConfig.ts
+++ apps/dapp/src/helpers/getRemoteConfig.ts
@@ -1,11 +1,11 @@
 import { configFile } from '@/constants/constants';
 const isLocal = process.env.NEXT_PUBLIC_IS_LOCAL
-const localDeployment = require('../../../contracts/.soroban/deployments.json')
 export const getRemoteConfig = async (network: string) => {
   if (isLocal === 'false') {
     const deployments = await configFile(network)
     return deployments
   } else if(isLocal === 'true') {
+    const localDeployment = require(`../../../contracts/.soroban/${network}.contracts.json`)
     return localDeployment
   }
 }
