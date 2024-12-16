0daab267d121ca80ebcb524dd1ffcaa5a2c05999
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-12-16 17:52:04 -0300

    🩹Fix build

--- apps/dapp/src/helpers/getRemoteConfig.ts
+++ apps/dapp/src/helpers/getRemoteConfig.ts
@@ -1,7 +1,6 @@
-import axios from 'axios'
-import localDeployment from '../../../contracts/.soroban/testnet.contracts.json';
 import { configFile } from '@/constants/constants';
 const isLocal = process.env.NEXT_PUBLIC_IS_LOCAL
+const localDeployment = require('../../../contracts/.soroban/deployments.json')
 export const getRemoteConfig = async (network: string) => {
   if(isLocal === 'true') {
     return localDeployment
