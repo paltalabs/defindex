9c8e92b7d33b780c9b211cdcffd2c15d9aa17c96
chopan123 <chopancatrileo@gmail.com>
2024-12-17 13:40:29 -0300

    referenced README for dapp

--- apps/dapp/src/helpers/getRemoteConfig.ts
+++ apps/dapp/src/helpers/getRemoteConfig.ts
@@ -4,10 +4,10 @@ import { configFile } from '@/constants/constants';
 const isLocal = process.env.NEXT_PUBLIC_IS_LOCAL
 
 export const getRemoteConfig = async (network: string) => {
-  if (isLocal === 'false') {
+  if (isLocal === 'false' || isLocal === undefined) {
     const deployments = await configFile(network)
     return deployments
-  } 
+  }
   /* else if(isLocal === 'true') {
     return localDeployment.ids
   } */
