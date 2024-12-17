cb549eec2de455111b64ea30aa9e07c9162610d7
coderipper <hello@joaquinsoza.dev>
2024-12-17 15:40:54 -0300

    release_fees and better returns

--- apps/contracts/vault/src/report.rs
+++ apps/contracts/vault/src/report.rs
@@ -1,6 +1,6 @@
-use soroban_sdk::{contracttype, Address, Env};
+use soroban_sdk::{contracttype, panic_with_error, Address, Env};
 
-use crate::{constants::MAX_BPS, storage::{get_report, set_report}};
+use crate::{constants::MAX_BPS, storage::{get_report, set_report}, ContractError};
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -11,7 +11,7 @@ pub struct Report {
 }
 
 impl Report {
-    pub fn lock_fee(&mut self, fee_rate: u32) -> i128 {
+    pub fn lock_fee(&mut self, fee_rate: u32) {
         let gains_or_losses = self.gains_or_losses;
         let numerator = gains_or_losses
             .checked_mul(fee_rate as i128)
@@ -20,35 +20,21 @@ impl Report {
 
         self.locked_fee += total_fee;
         self.gains_or_losses = 0;
-        
-        total_fee
+    }
+    
+    pub fn release_fee(&mut self, e: &Env, amount: i128) {
+        if self.locked_fee < amount {
+            panic_with_error!(e, ContractError::InsufficientManagedFunds);
+        }
+        self.locked_fee -= amount;
+        self.gains_or_losses += amount;
     }
 
-    pub fn report(&mut self, current_balance: i128) -> i128 {
+    pub fn report(&mut self, current_balance: i128) {
         let prev_balance = self.prev_balance;
         
         let gains_or_losses = current_balance - prev_balance;
         self.gains_or_losses += gains_or_losses;
         self.prev_balance = current_balance;
-
-        gains_or_losses
     }
-}
-
-/// Reports the gains or losses for a strategy based on the current balance.
-///
-/// # Arguments
-/// * `e` - A reference to the environment.
-/// * `strategy` - The address of the strategy.
-/// * `current_balance` - A reference to the current balance.
-///
-/// # Returns
-/// A tuple with the current balance and updated gains or losses.
-pub fn report(e: &Env, strategy: &Address, current_balance: &i128) -> (i128, i128) {
-    let mut report = get_report(e, strategy);
-    
-    let current_gains_or_losses = report.report(current_balance.clone());
-    set_report(e, strategy, report);
-
-    (current_balance.clone(), current_gains_or_losses)
 }
