9b1550ae82e8fa67723a0e0828243b305dca9494
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-12-18 11:56:20 -0300

    ✅Add several strategies one asset test

--- apps/contracts/vault/src/test/vault/initialize.rs
+++ apps/contracts/vault/src/test/vault/initialize.rs
@@ -1,9 +1,8 @@
 use soroban_sdk::{vec as sorobanvec, String, Vec};
 
 use crate::test::{
-    create_defindex_vault, create_strategy_params_token0, create_strategy_params_token1, defindex_vault::AssetStrategySet, DeFindexVaultTest
+    create_defindex_vault, create_strategy_params_token0, create_strategy_params_token1, create_hodl_strategy, defindex_vault::{AssetStrategySet, Strategy}, DeFindexVaultTest
 };
-
 
 #[test]
 fn get_roles() {
@@ -104,5 +103,75 @@ fn initialize_with_empty_asset_allocation() {
 // test initialzie with one asset and several strategies for the same asset
 #[test]
 fn with_one_asset_and_several_strategies() {
-    todo!();
+    let test = DeFindexVaultTest::setup();
+
+    let strategy_0 = create_hodl_strategy(&test.env, &test.token0.address.clone());
+    let strategy_1 = create_hodl_strategy(&test.env, &test.token0.address.clone());
+    let strategy_2 = create_hodl_strategy(&test.env, &test.token0.address.clone());
+
+    let strategy_params = sorobanvec![
+        &test.env,
+        Strategy {
+            name: String::from_str(&test.env, "Strategy 1"),
+            address: strategy_0.address.clone(),
+            paused: false,
+        },
+        Strategy {
+            name: String::from_str(&test.env, "Strategy 2"),
+            address: strategy_1.address.clone(),
+            paused: false,
+        },
+        Strategy {
+            name: String::from_str(&test.env, "Strategy 3"),
+            address: strategy_2.address.clone(),
+            paused: false,
+        },
+    ];
+
+    let assets: Vec<AssetStrategySet> = sorobanvec![
+        &test.env,
+        AssetStrategySet {
+            address: test.token0.address.clone(),
+            strategies: strategy_params.clone()
+        }
+    ];
+
+    let defindex_contract = create_defindex_vault(
+        &test.env,
+        assets,
+        test.manager.clone(),
+        test.emergency_manager.clone(),
+        test.vault_fee_receiver.clone(),
+        2000u32,
+        test.defindex_protocol_receiver.clone(),
+        test.defindex_factory.clone(),
+        String::from_str(&test.env, "dfToken"),
+        String::from_str(&test.env, "DFT"),
+    );
+
+    let manager_role = defindex_contract.get_manager();
+    let fee_receiver_role = defindex_contract.get_fee_receiver();
+    let emergency_manager_role = defindex_contract.get_emergency_manager();
+    
+    let vault_assets = defindex_contract.get_assets();
+    let asset = vault_assets.get(0).unwrap();
+    let vault_strategies = asset.strategies;
+    
+    let total_managed_funds = defindex_contract.fetch_total_managed_funds();
+    let current_invested_funds = defindex_contract.fetch_current_invested_funds().get(test.token0.address.clone());
+    let current_idle_funds = defindex_contract.fetch_current_idle_funds().get(test.token0.address.clone());
+    let total_amount = total_managed_funds.get(test.token0.address.clone()).unwrap().total_amount;
+
+    assert_eq!(manager_role, test.manager);
+    assert_eq!(fee_receiver_role, test.vault_fee_receiver);
+    assert_eq!(emergency_manager_role, test.emergency_manager);
+
+    assert_eq!(asset.address, test.token0.address);
+    assert_eq!(vault_assets.len(), 1);
+    assert_eq!(vault_strategies.len(), strategy_params.len());
+
+    assert_eq!(total_amount, 0i128);
+    assert_eq!(current_invested_funds, Some(0));
+    assert_eq!(current_idle_funds, Some(0)); 
+ 
 }
