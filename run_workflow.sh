ec02f73784f21150303a75260e62585261eb7bb6
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-01-08 20:06:53 -0300

    🧪Run workflow on script

---
+++ run_workflow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [ "$1" == "local" ]; then
+  echo "Running GitHub Actions workflow locally..."
+  act -W .github/workflows/test-contracts.yml -j build_and_test --container-architecture linux/amd64
+  #cd apps/contracts && make test
+elif [ "$1" == "remote" ]; then
+  echo "Running GitHub Actions workflow remote..."
+  echo "🥑🥑🥑🥑🥑🥑"
+  cd apps/contracts && make test || exit 1
+else
+  echo "Invalid parameter. Use 'local' or 'remote'."
+  exit 1
+fi
