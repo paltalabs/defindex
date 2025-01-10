b1b85c917686c6d10fbfec73c09418ff0c1755b7
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-01-10 12:08:18 -0300

    ✅Add unauthorized withdraw test

--- apps/contracts/strategies/blend/src/test/blend/error.rs
+++ apps/contracts/strategies/blend/src/test/blend/error.rs
@@ -5,8 +5,8 @@ use crate::test::{create_blend_pool, create_blend_strategy, BlendFixture, EnvTes
 use crate::BlendStrategyClient;
 use defindex_strategy_core::StrategyError;
 use sep_41_token::testutils::MockTokenClient;
-use soroban_sdk::testutils::Address as _;
-use soroban_sdk::{Address, Env};
+use soroban_sdk::testutils::{Address as _, MockAuth, MockAuthInvoke};
+use soroban_sdk::{Address, Env, IntoVal};
 
 extern crate std;
 
@@ -210,7 +210,7 @@ fn harvest_from_random_address(){
 }
 
 #[test]
-fn insufficient_balance(){
+fn withdraw_insufficient_balance(){
     // Setting up the environment
     let e = Env::default();
     e.budget().reset_unlimited();
@@ -288,7 +288,6 @@ fn insufficient_balance(){
 fn withdraw_zero_and_negative(){
     // Setting up the environment
     let e = Env::default();
-    e.budget().reset_unlimited();
     e.mock_all_auths();
     e.set_default_info();
 
@@ -353,4 +352,112 @@ fn withdraw_zero_and_negative(){
 
     let result = strategy_client.try_withdraw(&9_000 , &user_2, &user_2);
     assert_eq!(result, Err(Ok(StrategyError::AmountBelowMinDust)));
+}
+
+#[test]
+fn unauthorized_withdraw(){
+     // Setting up the environment
+    let e = Env::default();
+    e.set_default_info();
+
+    // Setting up the users
+    let admin = Address::generate(&e);
+    let user_2 = Address::generate(&e);
+    let user_3= Address::generate(&e);
+
+    // Setting up the assets
+    let blnd = e.register_stellar_asset_contract_v2(admin.clone());
+    let usdc = e.register_stellar_asset_contract_v2(admin.clone());
+    let xlm = e.register_stellar_asset_contract_v2(admin.clone());
+
+    // Setting up the token clients
+    let blnd_client = MockTokenClient::new(&e, &blnd.address());
+    let usdc_client = MockTokenClient::new(&e, &usdc.address());
+    let xlm_client = MockTokenClient::new(&e, &xlm.address());
+
+    // Setting up soroswap pool
+    let pool_admin = Address::generate(&e);
+    let amount_a = 100000000_0_000_000;
+    let amount_b = 50000000_0_000_000;
+    blnd_client.mock_all_auths().mint(&pool_admin, &amount_a);
+    usdc_client.mock_all_auths().mint(&pool_admin, &amount_b);
+    
+    let soroswap_router = create_soroswap_pool(
+        &e,
+        &pool_admin,
+        &blnd.address(),
+        &usdc.address(),
+        &amount_a,
+        &amount_b,
+    );
+
+    // End of setting up soroswap pool
+    let blend_fixture = BlendFixture::deploy(&e, &admin, &blnd.address(), &usdc.address());
+    
+    let pool = create_blend_pool(&e, &blend_fixture, &admin, &usdc_client, &xlm_client);
+    let strategy = create_blend_strategy(
+        &e,
+        &usdc.address(),
+        &pool,
+        &0u32,
+        &blnd.address(),
+        &soroswap_router.address,
+    );
+    let strategy_client = BlendStrategyClient::new(&e, &strategy); 
+
+    let starting_balance = 10_0_000_000i128;
+    usdc_client.mint(&user_2, &starting_balance);
+    strategy_client.mock_auths(
+        &[MockAuth {
+            address: &user_2,
+            invoke: &MockAuthInvoke {
+                contract: &strategy_client.address.clone(),
+                fn_name: "deposit",
+                args: (starting_balance.clone(), user_2.clone()).into_val(&e),
+                sub_invokes: &[
+                    MockAuthInvoke {
+                        contract: &usdc_client.address.clone(),
+                        fn_name: "transfer",
+                        args: (
+                            user_2.clone(),
+                            strategy_client.address.clone(),
+                            starting_balance.clone()
+                        ).into_val(&e),
+                        sub_invokes: &[],
+                    },
+                ],
+            },
+        }]
+    ).deposit(&starting_balance, &user_2);
+    
+    let balance = strategy_client.balance(&user_2);
+    let user_balance = usdc_client.balance(&user_2);
+    assert_eq!(balance, starting_balance);
+    assert_eq!(user_balance, 0);  
+
+    let withdraw_amount = 8_0_000_000i128;
+    let result = strategy_client.mock_auths(
+        &[MockAuth {
+            address: &user_3,
+            invoke: &MockAuthInvoke {
+                contract: &strategy_client.address.clone(),
+                fn_name: "withdraw",
+                args: (withdraw_amount.clone(), user_2.clone(), user_2.clone()).into_val(&e),
+                sub_invokes: &[
+                    MockAuthInvoke {
+                        contract: &usdc_client.address.clone(),
+                        fn_name: "transfer",
+                        args: (
+                            strategy_client.address.clone(),
+                            user_2.clone(),
+                            withdraw_amount.clone()
+                        ).into_val(&e),
+                        sub_invokes: &[],
+                    },
+                ],
+            },
+        }]
+    ).try_withdraw(&withdraw_amount, &user_2, &user_2);
+    
+    assert_eq!(result, Err(Err(soroban_sdk::InvokeError::Abort)));
 }
