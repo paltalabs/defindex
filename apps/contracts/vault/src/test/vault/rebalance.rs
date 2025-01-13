0ccff695fa1c4b9618cb4f5c0b0e6bbe6a3ac46d
coderipper <hello@joaquinsoza.dev>
2025-01-13 10:20:47 -0300

    added mock auths

--- apps/contracts/vault/src/test/vault/rebalance.rs
+++ apps/contracts/vault/src/test/vault/rebalance.rs
@@ -1,4 +1,5 @@
-use soroban_sdk::{vec as sorobanvec, Address, InvokeError, Map, String, Vec};
+use soroban_sdk::testutils::{MockAuth, MockAuthInvoke};
+use soroban_sdk::{vec as sorobanvec, Address, InvokeError, Map, String, Vec, IntoVal};
 
 use crate::test::defindex_vault::{ContractError, RolesDataKey};
 use crate::test::{
@@ -50,14 +51,14 @@ fn multi_instructions() {
 
     let users = DeFindexVaultTest::generate_random_users(&test.env, 1);
 
-    test.token_0_admin_client.mint(&users[0], &amount);
+    test.token_0_admin_client.mock_all_auths().mint(&users[0], &amount);
     let user_balance = test.token_0.balance(&users[0]);
     assert_eq!(user_balance, amount);
 
     let df_balance = defindex_contract.balance(&users[0]);
     assert_eq!(df_balance, 0i128);
 
-    defindex_contract.deposit(
+    defindex_contract.mock_all_auths().deposit(
         &sorobanvec![&test.env, amount],
         &sorobanvec![&test.env, amount],
         &users[0],
@@ -75,7 +76,21 @@ fn multi_instructions() {
         ),
     ];
 
-    defindex_contract.rebalance(&test.rebalance_manager, &invest_instructions);
+    defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                invest_instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .rebalance(&test.rebalance_manager, &invest_instructions);
 
     let vault_balance = test.token_0.balance(&defindex_contract.address);
     assert_eq!(vault_balance, 0);
@@ -97,7 +112,21 @@ fn multi_instructions() {
         ),
     ];
 
-    defindex_contract.rebalance(&test.rebalance_manager, &instructions);
+    defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .rebalance(&test.rebalance_manager, &instructions);
 
     let vault_balance = test.token_0.balance(&defindex_contract.address);
     assert_eq!(vault_balance, instruction_amount_1);
@@ -143,14 +172,14 @@ fn one_instruction() {
 
     let users = DeFindexVaultTest::generate_random_users(&test.env, 1);
 
-    test.token_0_admin_client.mint(&users[0], &amount);
+    test.token_0_admin_client.mock_all_auths().mint(&users[0], &amount);
     let user_balance = test.token_0.balance(&users[0]);
     assert_eq!(user_balance, amount);
 
     let df_balance = defindex_contract.balance(&users[0]);
     assert_eq!(df_balance, 0i128);
 
-    defindex_contract.deposit(
+    defindex_contract.mock_all_auths().deposit(
         &sorobanvec![&test.env, amount],
         &sorobanvec![&test.env, amount],
         &users[0],
@@ -168,7 +197,21 @@ fn one_instruction() {
         ),
     ];
 
-    defindex_contract.rebalance(&test.rebalance_manager, &invest_instructions);
+    defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                invest_instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .rebalance(&test.rebalance_manager, &invest_instructions);
 
     let vault_balance = test.token_0.balance(&defindex_contract.address);
     assert_eq!(vault_balance, 0);
@@ -185,61 +228,24 @@ fn one_instruction() {
         ),
     ];
 
-    defindex_contract.rebalance(&test.rebalance_manager, &instructions);
+    defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .rebalance(&test.rebalance_manager, &instructions);
 
     let vault_balance = test.token_0.balance(&defindex_contract.address);
     assert_eq!(vault_balance, instruction_amount_0);
-}
-
-#[test]
-fn empty_instructions() {
-    let test = DeFindexVaultTest::setup();
-
-    let strategy_params_token_0 = create_strategy_params_token_0(&test);
-    let assets: Vec<AssetStrategySet> = sorobanvec![
-        &test.env,
-        AssetStrategySet {
-            address: test.token_0.address.clone(),
-            strategies: strategy_params_token_0.clone()
-        }
-    ];
-    let mut roles: Map<u32, Address> = Map::new(&test.env);
-    roles.set(RolesDataKey::Manager as u32, test.manager.clone());
-    roles.set(RolesDataKey::EmergencyManager as u32, test.emergency_manager.clone());
-    roles.set(RolesDataKey::VaultFeeReceiver as u32, test.vault_fee_receiver.clone());
-    roles.set(RolesDataKey::RebalanceManager as u32, test.rebalance_manager.clone());
-
-    let mut name_symbol: Map<String, String> = Map::new(&test.env);
-    name_symbol.set(String::from_str(&test.env, "name"), String::from_str(&test.env, "dfToken"));
-    name_symbol.set(String::from_str(&test.env, "symbol"), String::from_str(&test.env, "DFT"));
-
-    let defindex_contract = create_defindex_vault(
-        &test.env,
-        assets,
-        roles,
-        2000u32,
-        test.defindex_protocol_receiver.clone(),
-        2500u32,
-        test.defindex_factory.clone(),
-        test.soroswap_router.address.clone(),
-        name_symbol,
-        true
-    );
-    
-    let amount: i128 = 987654321;
-    let users = DeFindexVaultTest::generate_random_users(&test.env, 1);
-    test.token_0_admin_client.mint(&users[0], &amount);
-    let vault_balance = defindex_contract.balance(&users[0]);
-    assert_eq!(vault_balance, 0i128);
-
-    defindex_contract.deposit(
-        &sorobanvec![&test.env, amount],
-        &sorobanvec![&test.env, amount],
-        &users[0],
-        &false,
-    );
-    let df_balance = defindex_contract.balance(&users[0]);
-    assert_eq!(df_balance, amount - 1000);
 }
 
 #[test]
@@ -333,7 +339,7 @@ fn insufficient_balance() {
     
     let amount: i128 = 987654321;
     let users = DeFindexVaultTest::generate_random_users(&test.env, 1);
-    test.token_0_admin_client.mint(&users[0], &amount);
+    test.token_0_admin_client.mock_all_auths().mint(&users[0], &amount);
 
     //Balance should be 0
     let vault_balance = defindex_contract.balance(&users[0]);
@@ -345,7 +351,21 @@ fn insufficient_balance() {
         Instruction::Unwind(test.strategy_client_token_0.address.clone(), amount + 1),
     ];
 
-    let withdraw_no_funds = defindex_contract.try_rebalance(&test.rebalance_manager, &withdraw_no_funds_instructions);
+    let withdraw_no_funds = defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                withdraw_no_funds_instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .try_rebalance(&test.rebalance_manager, &withdraw_no_funds_instructions);
     assert_eq!(
         withdraw_no_funds,
         Err(Ok(ContractError::StrategyWithdrawError))
@@ -356,7 +376,21 @@ fn insufficient_balance() {
         Instruction::Invest(test.strategy_client_token_0.address.clone(), 1),
     ];
 
-    let invest_no_funds = defindex_contract.try_rebalance(&test.rebalance_manager, &invest_no_funds_instructions);
+    let invest_no_funds = defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                invest_no_funds_instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .try_rebalance(&test.rebalance_manager, &invest_no_funds_instructions);
 
     //Contract should fail with error #10 no balance or panic the test
     if invest_no_funds != Err(Err(InvokeError::Contract(10))) {
@@ -364,7 +398,7 @@ fn insufficient_balance() {
     }
 
     //Deposit 987654321 stroops
-    defindex_contract.deposit(
+    defindex_contract.mock_all_auths().deposit(
         &sorobanvec![&test.env, amount],
         &sorobanvec![&test.env, amount],
         &users[0],
@@ -379,7 +413,21 @@ fn insufficient_balance() {
         Instruction::Unwind(test.strategy_client_token_0.address.clone(), amount + 1),
     ];
 
-    let rebalance = defindex_contract.try_rebalance(&test.rebalance_manager, &withdraw_instructions);
+    let rebalance = defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                withdraw_instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .try_rebalance(&test.rebalance_manager, &withdraw_instructions);
     assert_eq!(rebalance, Err(Ok(ContractError::StrategyWithdrawError)));
 
     let invest_instructions = sorobanvec![
@@ -388,7 +436,21 @@ fn insufficient_balance() {
     ];
 
     //Contract should fail with error #10 no balance
-    let rebalance = defindex_contract.try_rebalance(&test.rebalance_manager, &invest_instructions);
+    let rebalance = defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                invest_instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .try_rebalance(&test.rebalance_manager, &invest_instructions);
     if rebalance == Err(Err(InvokeError::Contract(10))) {
         return;
     } else {
@@ -444,10 +506,10 @@ fn swap_exact_in() {
 
     let users = DeFindexVaultTest::generate_random_users(&test.env, 2);
 
-    test.token_0_admin_client.mint(&users[0], &amount0);
-    test.token_1_admin_client.mint(&users[0], &amount1);
+    test.token_0_admin_client.mock_all_auths().mint(&users[0], &amount0);
+    test.token_1_admin_client.mock_all_auths().mint(&users[0], &amount1);
 
-    defindex_contract.deposit(
+    defindex_contract.mock_all_auths().deposit(
         &sorobanvec![&test.env, amount0, amount1],
         &sorobanvec![&test.env, amount0, amount1],
         &users[0],
@@ -514,7 +576,21 @@ fn swap_exact_in() {
         ),
     ];
 
-    defindex_contract.rebalance(&test.rebalance_manager, &instructions);
+    defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .rebalance(&test.rebalance_manager, &instructions);
 
     // check total managed funds
     let mut total_managed_funds_expected = Map::new(&test.env);
@@ -604,10 +680,10 @@ fn swap_exact_out() {
 
     let users = DeFindexVaultTest::generate_random_users(&test.env, 2);
 
-    test.token_0_admin_client.mint(&users[0], &amount0);
-    test.token_1_admin_client.mint(&users[0], &amount1);
+    test.token_0_admin_client.mock_all_auths().mint(&users[0], &amount0);
+    test.token_1_admin_client.mock_all_auths().mint(&users[0], &amount1);
 
-    defindex_contract.deposit(
+    defindex_contract.mock_all_auths().deposit(
         &sorobanvec![&test.env, amount0, amount1],
         &sorobanvec![&test.env, amount0, amount1],
         &users[0],
@@ -676,7 +752,21 @@ fn swap_exact_out() {
         ),
     ];
 
-    defindex_contract.rebalance(&test.rebalance_manager, &instructions);
+    defindex_contract
+    .mock_auths(&[MockAuth {
+        address: &test.rebalance_manager.clone(),
+        invoke: &MockAuthInvoke {
+            contract: &defindex_contract.address.clone(),
+            fn_name: "rebalance",
+            args: (
+                test.rebalance_manager.clone(),
+                instructions.clone(),
+            )
+                .into_val(&test.env),
+            sub_invokes: &[],
+        },
+    }])
+    .rebalance(&test.rebalance_manager, &instructions);
 
     // check total managed funds
     let mut total_managed_funds_expected = Map::new(&test.env);
