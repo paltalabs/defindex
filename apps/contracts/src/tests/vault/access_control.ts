1b89b1012752c5e7b2086753ac0c9dd10041b854
coderipper <hello@joaquinsoza.dev>
2025-01-30 19:22:51 -0400

    fixed ts scripts for testing acces_control

--- apps/contracts/src/tests/vault/access_control.ts
+++ apps/contracts/src/tests/vault/access_control.ts
@@ -1,18 +1,14 @@
 import { Keypair } from "@stellar/stellar-sdk";
 import { AddressBook } from "../../utils/address_book.js";
-import { CreateVaultParams, manager, queueVaultManager, setEmergencyManager, setFeeReceiver, setRebalanceManager, setVaultManager } from "../vault.js";
-import { deployDefindexVault } from "./utils.js";
-import { green, purple, red } from "../common.js";
 import { airdropAccount } from "../../utils/contract.js";
+import { green, purple, red } from "../common.js";
+import { CreateVaultParams, manager, setEmergencyManager, setFeeReceiver, setRebalanceManager, setVaultManager } from "../vault.js";
+import { deployDefindexVault } from "./utils.js";
 
 /* 
 // Access control tests:
-  - [x] try setManager without previous queued manager
-  - [x] try queueManager from unauthorized
-  - [x] queueManager
-  - [x] try setManager before time is up
-  - [ ] try setManager from unauthorized
-  - [ ] setManager
+  - [x] try setManager from unauthorized
+  - [x] setManager
 
   - [x] try setRebalanceManager from unauthorized
   - [x] setRebalanceManager
@@ -30,87 +26,8 @@ export async function testAccessControl(addressBook: AddressBook, params: Create
   } = await deployDefindexVault(addressBook, params);
   if (!vault_address) throw new Error("Vault was not deployed");
 
-  //Try set manager without previous queued manager
-  try {
-    console.log(purple, "---------------------------------------");
-    console.log(purple, "Set manager");
-    console.log(purple, "---------------------------------------");
-    const { instructions, readBytes, writeBytes } = await setVaultManager(vault_address, user);
-    return { instructions, readBytes, writeBytes };
-  } catch (error: any ) {
-    if (error.toString().includes("HostError: Error(Contract, #134)")) {
-      console.log(green, "------------------------------------------------------------------");
-      console.log(green, "| Set manager without previous queued manager failed as expected |");
-      console.log(green, "------------------------------------------------------------------");
-    } else {
-      throw Error(error);
-    }
-  }
-
-  //Try queue manager from unauthorized
-  try {
-    console.log(purple, "---------------------------------------");
-    console.log(purple, "Try queue manager from unauthorized");
-    console.log(purple, "---------------------------------------");
-    const random_user = Keypair.random();
-    await airdropAccount(random_user);
-    const  {result} = await queueVaultManager(vault_address, random_user, random_user);
-    if( result !== false){
-      throw Error("Queue manager from unauthorized validation failed");
-    } else if (result === false) {
-      console.log(green, "------------------------------------------------------");
-      console.log(green, "| Queue manager from unauthorized failed as expected |");
-      console.log(green, "------------------------------------------------------");
-    }
-  } catch (error: any) {
-    throw Error(error);
-  }
-
   const new_manager = Keypair.random();
   await airdropAccount(new_manager);
-  // Queue manager
-  const {
-    result,
-    instructions: queue_instructions,
-    readBytes: queue_read_bytes,
-    writeBytes: queue_write_bytes
-  } = await (async ()=>{
-      try {
-        console.log(purple, "---------------------------------------");
-        console.log(purple, "Queue manager");
-        console.log(purple, "---------------------------------------");
-        const  {result, instructions, readBytes, writeBytes } = await queueVaultManager(vault_address, manager, new_manager);
-        if( result !== true){
-          console.log(green, "-----------------------------------");
-          console.log(green, "| Queue manager set successufully |");
-          console.log(green, "-----------------------------------");
-        } else if (result === false) {
-          console.error(red, "Queue manager from unauthorized validation failed");
-        }
-        return {result, instructions, readBytes, writeBytes};
-      } catch (error: any) {
-        console.error(red, error);
-        return {result: false, instructions: 0, readBytes: 0, writeBytes: 0};
-      }
-    }
-  )();
-
-  //Try setManager before time is up
-  try {
-    console.log(purple, "---------------------------------------");
-    console.log(purple, "Set manager before time");
-    console.log(purple, "---------------------------------------");
-    const { instructions, readBytes, writeBytes } = await setVaultManager(vault_address, user);
-    return { instructions, readBytes, writeBytes };
-  } catch (error: any ) {
-    if (error.toString().includes("HostError: Error(Contract, #133)")) {
-      console.log(green, "----------------------------------------------");
-      console.log(green, "| Set manager before time failed as expected |");
-      console.log(green, "----------------------------------------------");
-    } else {
-      throw Error(error);
-    }
-  } 
 
   // Try setRebalanceManager from unauthorized
   try {
@@ -243,11 +160,55 @@ export async function testAccessControl(addressBook: AddressBook, params: Create
       console.error(red, error);
       return {result: false, instructions: 0, readBytes: 0, writeBytes: 0};
     }
-  } )();
+    })();
+  
+    //Try set manager from unauthorized
+    try {
+      console.log(purple, "---------------------------------------");
+      console.log(purple, "Try set manager from unauthorized");
+      console.log(purple, "---------------------------------------");
+      const random_user = Keypair.random();
+      await airdropAccount(random_user);
+      const  {result} = await setVaultManager(vault_address, random_user, random_user);
+      if( result !== false){
+        throw Error("Set manager from unauthorized validation failed");
+      } else if (result === false) {
+        console.log(green, "------------------------------------------------------");
+        console.log(green, "| Set manager from unauthorized failed as expected |");
+        console.log(green, "------------------------------------------------------");
+      }
+    } catch (error: any) {
+      throw Error(error);
+    }
+  
+    //setManager succesfully
+    const {
+      instructions: set_manager_instructions, 
+      readBytes: set_manager_read_bytes, 
+      writeBytes: set_manager_write_bytes
+    } = await (async () => {
+      try {
+        console.log(purple, "---------------------------------------");
+        console.log(purple, "setRebalanceManager");
+        console.log(purple, "---------------------------------------");
+        const {result, instructions, readBytes, writeBytes}  = await setVaultManager(vault_address, new_manager, manager);
+        if( result === null){
+          console.log(green, "--------------------------------------");
+          console.log(green, "| Manager set sucessfully  |");
+          console.log(green, "--------------------------------------");
+        } else if (result === false) {
+          throw Error("Set manager failed");
+        }
+        return {result, instructions, readBytes, writeBytes};
+      } catch (error: any) {
+        console.error(red, error);
+        return {result: false, instructions: 0, readBytes: 0, writeBytes: 0};
+      } 
+    } )();
 
   const tests_status = {
-    queue: {
-      status: !!queue_instructions && !!queue_read_bytes && !!queue_write_bytes,
+    set_manager: {
+      status: !!set_manager_instructions && !!set_manager_read_bytes && !!set_manager_write_bytes,
     },
     set_rebalance_manager: {
       status: !!set_rebalance_manager_instructions && !!set_rebalance_manager_read_bytes && !!set_rebalance_manager_write_bytes,
@@ -261,11 +222,11 @@ export async function testAccessControl(addressBook: AddressBook, params: Create
   }
 
   const budgetData = {
-    queue: {
-      status: tests_status.queue.status ? `success`: `failed`,
-      instructions: queue_instructions,
-      readBytes: queue_read_bytes,
-      writeBytes: queue_write_bytes,
+    set_manager: {
+      status: tests_status.set_manager.status ? `success`: `failed`,
+      instructions: set_manager_instructions,
+      readBytes: set_manager_read_bytes,
+      writeBytes: set_manager_write_bytes,
     },
     set_rebalance_manager: {
       status: tests_status.set_rebalance_manager.status ? `success`: `failed`,
