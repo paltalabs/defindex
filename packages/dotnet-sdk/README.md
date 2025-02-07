a97e39bbed9bb30c813ffe26e81ae1b7fa059cf9
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-02-07 10:22:23 -0300

    Revert "Merge branch 'feat/dotnet_hello' of https://github.com/paltalabs/defindex into feat/refactor-workflows"
    This reverts commit 9a3340723d0260d3590b822b591eac1e42e0b695, reversing
    changes made to 3780421a3f4281bee7aa8bfdf5597ccf0350b76d.

--- packages/dotnet-sdk/README.md
+++
@@ -1,9 +0,0 @@
-# DeFindex .NET SDK
-
-.NET SDK for the DeFindex protocol.
-
-## Installation
-
-## Usage
-
-Basic usage examples will go here. 
--- packages/dotnet-sdk/src/Interfaces/IDefindexSdk.cs
+++
@@ -1,7 +0,0 @@
-namespace DeFindex.Sdk.Interfaces;
-
-public interface IDefindexSdk
-{
-    Task<bool> InitializeAsync();
-    // Add more interface methods as needed
-} 
