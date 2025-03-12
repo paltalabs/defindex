5d9db1e612c49a315065daf963832556732b7efd
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-03-12 15:13:19 -0300

    ✅Fix limits on integration test

--- apps/contracts/integration-test/src/test/limits/rebalance.rs
+++ apps/contracts/integration-test/src/test/limits/rebalance.rs
@@ -762,7 +762,7 @@ fn asset_one_strategy_blend_rebalance() {
         &false
     );
 
-    let num_investments = 3;
+    let num_investments = 2;
 
     let mut invest_instructions = svec![&setup.env];
     for _ in 0..num_investments {
@@ -908,7 +908,7 @@ fn asset_one_strategy_blend_rebalance_panic_invest() {
         &false
     );
 
-    let num_investments = 5;
+    let num_investments = 4;
 
     let mut invest_instructions = svec![&setup.env];
     for _ in 0..num_investments {
@@ -1053,7 +1053,7 @@ fn asset_one_strategy_blend_rebalance_panic_unwind() {
     vault_contract.rebalance(&manager, &invest_instructions);
 
     let balance_on_strategy = strategy_contract.balance(&vault_contract.address);
-    let num_unwinds = 6;
+    let num_unwinds = 5;
 
     let mut unwinds_instructions = svec![&setup.env];
     for _ in 0..num_unwinds {
