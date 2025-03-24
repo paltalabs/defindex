491d9f81608e7d01053972eedc5c45859cc21377
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-03-24 16:26:43 -0300

    ♻️Refactor ConfirmDeployModal

--- apps/dapp/src/components/DeployVault/ConfirmDelpoyModal.tsx
+++ apps/dapp/src/components/DeployVault/ConfirmDelpoyModal.tsx
@@ -1,26 +1,25 @@
+import React, { useContext, useEffect, useState } from "react";
 import { useSorobanReact } from "@soroban-react/core";
 import {
   Address,
-  nativeToScVal,
   scValToNative,
   xdr,
 } from "@stellar/stellar-sdk";
-import { randomBytes } from "crypto";
-import { useContext, useEffect, useState } from "react";
 
-import { pushVault } from '@/store/lib/features/walletStore';
-import { useAppDispatch, useAppSelector } from "@/store/lib/storeHooks";
+import { useAppDispatch, useAppSelector } from "@/store/lib/storeHooks"
+import { pushVault } from '@/store/lib/features/walletStore'
 import { NewVaultState, VaultData } from "@/store/lib/types";
 
+import { useFactoryCallback, FactoryMethod } from '@/hooks/useFactory'
 import { ModalContext, TransactionStatusModalStatus } from "@/contexts";
-import { FactoryMethod, useFactoryCallback } from '@/hooks/useFactory';
 
-import { soroswapRouter } from "@/constants/constants";
-import { useVault } from "@/hooks/useVault";
-import { resetNewVault } from "@/store/lib/features/vaultStore";
-import { Button } from "@chakra-ui/react";
-import { DialogBody, DialogCloseTrigger, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
 import { AccordionItems, FormControlInterface, VaultPreview } from "./VaultPreview";
+import { DialogBody, DialogCloseTrigger, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
+import { Button } from "@chakra-ui/react"
+import { resetNewVault } from "@/store/lib/features/vaultStore";
+import { useVault } from "@/hooks/useVault";
+import { getAssetParamsSCVal, getCreateDeFindexVaultDepositParams, getCreateDeFindexVaultParams } from "@/helpers/vault";
+import { soroswapRouter } from "@/constants/constants";
 
 interface Status {
   isSuccess: boolean,
@@ -51,6 +50,7 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
   const emergencyManagerString = useAppSelector(state => state.newVault.emergencyManager)
   const rebalanceManagerString = useAppSelector(state => state.newVault.rebalanceManager)
   const feeReceiverString = useAppSelector(state => state.newVault.feeReceiver)
+  const [routerAddress, setRouterAddress] = useState<string>('')
   const { transactionStatusModal: txModal, deployVaultModal: deployModal } = useContext(ModalContext);
   const dispatch = useAppDispatch();
   const { getFees } = useVault()
@@ -85,15 +85,7 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
   const [buttonText, setButtonText] = useState<string>('')
   const [accordionValue, setAccordionValue] = useState<AccordionItems[]>([AccordionItems.STRATEGY_DETAILS])
   const [formControl, setFormControl] = useState<FormControlInterface>({
-    manager: {
-      isValid: undefined,
-      value: undefined
-    },
     emergencyManager: {
-      isValid: undefined,
-      value: undefined
-    },
-    rebalanceManager: {
       isValid: undefined,
       value: undefined
     },
@@ -101,8 +93,16 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
       isValid: undefined,
       value: undefined
     },
-    vaultShare: 0,
+    manager: {
+      isValid: undefined,
+      value: undefined
+    },
+    rebalanceManager: {
+      isValid: undefined,
+      value: undefined
+    },
     upgradable: true,
+    vaultShare: 0
   })
 
   useEffect(() => {
@@ -118,7 +118,23 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
 
   }, [managerString, emergencyManagerString, rebalanceManagerString, feeReceiverString, indexShare])
 
+  useEffect(() => {
+    switch (activeChain?.id.toLowerCase()) {
+      case 'testnet':
+        setRouterAddress(soroswapRouter.testnet)
+        break;
+      case 'public':
+        setRouterAddress(soroswapRouter.mainnet)
+        break;
+      default:
+        setRouterAddress(soroswapRouter.testnet)
+        break;
+    }
+  }, [activeChain?.id])
+
   const deployDefindex = async () => {
+    let result: any;
+
     if (managerString === '' || emergencyManagerString === '' || rebalanceManagerString === '') {
       console.log('please fill manager config')
       setAccordionValue([AccordionItems.MANAGER_CONFIGS])
@@ -132,128 +148,29 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
     deployModal.setIsOpen(false)
     txModal.initModal();
 
-    const vaultName = nativeToScVal(indexName, { type: "string" })
-    const vaultSymbol = nativeToScVal(indexSymbol, { type: "string" })
-    const vaultShare = nativeToScVal(indexShare, { type: "u32" })
     const emergencyManager = new Address(emergencyManagerString)
     const rebalanceManager = new Address(rebalanceManagerString)
     const feeReceiver = new Address(feeReceiverString)
     const manager = new Address(managerString)
-    const salt = randomBytes(32)
-
-    /*
-        pub struct AssetAllocation {
-          pub address: Address,
-          pub strategies: Vec<Strategy>,
-        } 
-        pub struct Strategy {
-          pub address: Address,
-          pub name: String,
-          pub paused: bool,
-        }
-    */
-
-    const assetParamsScVal = newVault.assets.map((asset) => {
-      const strategyParamsScVal = asset.strategies.map((param) => {
-        return xdr.ScVal.scvMap([
-          new xdr.ScMapEntry({
-            key: xdr.ScVal.scvSymbol('address'),
-            val: new Address(param.address).toScVal(),
-          }),
-          new xdr.ScMapEntry({
-            key: xdr.ScVal.scvSymbol('name'),
-            val: nativeToScVal(param.name, { type: "string" }),
-          }),
-          new xdr.ScMapEntry({
-            key: xdr.ScVal.scvSymbol('paused'),
-            val: nativeToScVal(false, { type: "bool" }),
-          }),
-        ]);
-      });
-      const strategyParamsScValVec = xdr.ScVal.scvVec(strategyParamsScVal);
-      return xdr.ScVal.scvMap([
-        new xdr.ScMapEntry({
-          key: xdr.ScVal.scvSymbol('address'),
-          val: new Address(asset.address).toScVal(),
-        }),
-        new xdr.ScMapEntry({
-          key: xdr.ScVal.scvSymbol('strategies'),
-          val: strategyParamsScValVec,
-        }),
-      ]);
-    });
-    const assetParamsScValVec = xdr.ScVal.scvVec(assetParamsScVal);
-    const amountsScVal = newVault.assets.map((asset, index) => {
-      const parsedAmount = newVault.assets[index]?.amount || 0;
-      const truncatedAmount = Math.floor(parsedAmount * 1e7) / 1e7;
-      const convertedAmount = Number(truncatedAmount) * Math.pow(10, 7)
-      if (newVault.assets[index]?.amount === 0) return nativeToScVal(0, { type: "i128" });
-      return nativeToScVal(convertedAmount, { type: "i128" });
-    });
-    /*  const amountsScVal = newVault.amounts.map((amount) => {
-       return nativeToScVal((amount * Math.pow(10, 7)), { type: "i128" });
-     }); */
-    const amountsScValVec = xdr.ScVal.scvVec(amountsScVal);
-    /*  fn create_defindex_vault(
-     emergency_manager: address, 
-     fee_receiver: address, 
-     vault_share: u32, 
-     vault_name: string, 
-     vault_symbol: string, 
-     manager: address, 
-     assets: vec<AssetAllocation>, 
-     salt: bytesn<32>) -> result<address,FactoryError>
-*/
-    let result: any;
-
-    const roles = xdr.ScVal.scvMap([
-      new xdr.ScMapEntry({
-        key: xdr.ScVal.scvU32(0),
-        val: emergencyManager.toScVal(),
-      }),
-      new xdr.ScMapEntry({
-        key: xdr.ScVal.scvU32(1),
-        val: feeReceiver.toScVal(),
-      }),
-      new xdr.ScMapEntry({
-        key: xdr.ScVal.scvU32(2),
-        val: manager.toScVal(),
-      }),
-      new xdr.ScMapEntry({
-        key: xdr.ScVal.scvU32(3),
-        val: rebalanceManager.toScVal(),
-      }),
-    ]);
-
-    const nameSymbol = xdr.ScVal.scvMap([
-      new xdr.ScMapEntry({
-        key: xdr.ScVal.scvString("name"),
-        val: nativeToScVal(vaultName, { type: "string" }),
-      }),
-      new xdr.ScMapEntry({
-        key: xdr.ScVal.scvString("symbol"),
-        val: nativeToScVal(vaultSymbol, { type: "string" }),
-      }),
-    ]);
-
-    const upgradable = nativeToScVal(true)
-
-    const soroswapRouterAddress = new Address(activeChain?.id == "testnet" ? soroswapRouter.testnet : soroswapRouter.mainnet);
+    const assetParamsScVal = getAssetParamsSCVal(newVault.assets);
 
     if (newVault.assets[0]?.amount === undefined) {
-      const createDefindexParams: xdr.ScVal[] = [
-        roles,
-        vaultShare,
-        assetParamsScValVec,
-        nativeToScVal(salt),
-        soroswapRouterAddress.toScVal(),
-        nameSymbol,
-        upgradable,
-      ];
+      const vault_params: xdr.ScVal[] = getCreateDeFindexVaultParams(
+        emergencyManager.toString(),
+        rebalanceManager.toString(),
+        feeReceiver.toString(),
+        manager.toString(),
+        indexShare,
+        indexName,
+        indexSymbol,
+        assetParamsScVal,
+        routerAddress,
+        true
+      );
       try {
         result = await factory(
           FactoryMethod.CREATE_DEFINDEX_VAULT,
-          createDefindexParams,
+          vault_params,
           true,
         )
       }
@@ -265,23 +182,24 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
       }
     } else if (newVault.assets[0]?.amount! > 0) {
       if (!address) throw new Error('Address not found')
-      const caller = new Address(address);
-      const createDefindexParams: xdr.ScVal[] = [
-        caller.toScVal(),
-        roles,
-        vaultShare,
-        assetParamsScValVec,
-        nativeToScVal(salt),
-        soroswapRouterAddress.toScVal(),
-        nameSymbol,
-        upgradable,
-        amountsScValVec,
-      ];
-
+      const createDefindexDepositParams: xdr.ScVal[] = getCreateDeFindexVaultDepositParams(
+        address,
+        emergencyManager.toString(),
+        feeReceiver.toString(),
+        rebalanceManager.toString(),
+        manager.toString(),
+        indexShare,
+        indexName,
+        indexSymbol,
+        assetParamsScVal,
+        routerAddress,
+        true,
+        newVault.assets
+      )
       try {
         result = await factory(
           FactoryMethod.CREATE_DEFINDEX_VAULT_DEPOSIT,
-          createDefindexParams,
+          createDefindexDepositParams,
           true,
         )
       }
@@ -311,7 +229,7 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
       TVL: 0,
       totalSupply: 0,
       idleFunds: idleFunds,
-      investedFunds: [{ address: '', amount: 0 }],
+      investedFunds: investedFunds || [{ address: '', amount: 0 }],
       fees: fees,
     }
     await txModal.handleSuccess(result.txHash);
@@ -320,9 +238,7 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
     return result;
   }
 
-  //to-do Use chakra-ui stepper component
   return (
-
     <>
       <DialogHeader>
         <DialogTitle>
@@ -349,6 +265,5 @@ export const ConfirmDelpoyModal = ({ isOpen, onClose }: { isOpen: boolean, onClo
         </Button>
       </DialogFooter>
     </>
-
   )
 }
