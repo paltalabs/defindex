e6f5295342f10798b6f9fa25cc15ca7bab985a19
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-04-04 12:35:48 -0300

    🔥Removed unused import

--- apps/dapp/app/page.tsx
+++ apps/dapp/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { HStack } from '@chakra-ui/react'
-import { useSorobanReact } from 'stellar-react'
 import ManageVaults from '@/components/ManageVaults/ManageVaults';
 import {
   ArcElement,
@@ -11,7 +10,6 @@ ChartJS.register(ArcElement);
 
 
 export default function Home() {
-  const { address } = useSorobanReact()
   return (
     <HStack w={'full'} alignContent={'center'} justifyContent={'center'}>
       <ManageVaults />
