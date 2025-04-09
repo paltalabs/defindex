80f8fc867ceca0beb5c6065aba68842ddfedcacd
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-04-09 16:46:19 -0400

    🧪Test deploy w/ invalid treshold

--- apps/contracts/strategies/blend/src/test/blend/error.rs
+++ apps/contracts/strategies/blend/src/test/blend/error.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 use crate::test::blend::soroswap_setup::create_soroswap_pool;
 use crate::test::{
-    create_blend_pool, create_blend_strategy, BlendFixture, EnvTestUtils, ONE_DAY_IN_SECONDS,
+    create_blend_pool, create_blend_strategy, register_blend_strategy, BlendFixture, EnvTestUtils, ONE_DAY_IN_SECONDS
 };
 use crate::reserves;
 
@@ -725,4 +725,50 @@ fn set_keeper_unauthorized() {
     
     // Verify the keeper hasn't changed
     assert_eq!(strategy_client.get_keeper(), keeper);
+}
+
+#[test]
+#[should_panic(expected = "Reward threshold must be positive")] // Unauthorized
+fn deploy_contract_invalid_reward_treshold() {
+    let e = Env::default();
+    e.set_default_info();
+    let admin = Address::generate(&e);
+    let keeper = Address::generate(&e);
+
+    let blnd = e.register_stellar_asset_contract_v2(admin.clone());
+    let usdc = e.register_stellar_asset_contract_v2(admin.clone());
+    let xlm = e.register_stellar_asset_contract_v2(admin.clone());
+
+    let blnd_client = MockTokenClient::new(&e, &blnd.address());
+    let usdc_client = MockTokenClient::new(&e, &usdc.address());
+    let xlm_client = MockTokenClient::new(&e, &xlm.address());
+
+    let pool_admin = Address::generate(&e);
+    let amount_a = 100000000_0_000_000;
+    let amount_b = 50000000_0_000_000;
+    blnd_client.mock_all_auths().mint(&pool_admin, &amount_a);
+    usdc_client.mock_all_auths().mint(&pool_admin, &amount_b);
+
+    let soroswap_router = create_soroswap_pool(
+        &e,
+        &pool_admin,
+        &blnd.address(),
+        &usdc.address(),
+        &amount_a,
+        &amount_b,
+    );
+    let blend_fixture = BlendFixture::deploy(&e, &admin, &blnd.address(), &usdc.address());
+
+    let pool = create_blend_pool(&e, &blend_fixture, &admin, &usdc_client, &xlm_client, &blnd_client);
+
+    let reward_threshold = -10i128;
+    let _ = register_blend_strategy(
+        &e,
+        &usdc.address(),
+        &pool,
+        &blnd.address(),
+        &soroswap_router.address,
+        reward_threshold,
+        &keeper,
+    );
 }
