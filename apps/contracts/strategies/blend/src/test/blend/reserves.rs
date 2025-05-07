911f0a3fd0de03f45b2077ffe93d4ed34c4723ca
chopan123 <chopancatrileo@gmail.com>
2025-05-07 17:31:48 -0400

    Added price per share to event to help calculate APY

--- apps/contracts/strategies/blend/src/test/blend/reserves.rs
+++ apps/contracts/strategies/blend/src/test/blend/reserves.rs
@@ -1,8 +1,26 @@
-use defindex_strategy_core::StrategyError;
-
-use super::utils::create_generic_strategy;
-use crate::reserves;
+// External crates
+use soroban_sdk::{
+    symbol_short,
+    Bytes,
+    String,
+    FromVal,
+    IntoVal,
+    testutils::Events,
+};
 extern crate std;
+
+// Internal crates
+use defindex_strategy_core::{
+    StrategyError,
+    event::HarvestEvent,
+};
+
+// Local imports
+use super::utils::{create_generic_strategy, mint_and_deposit_to_strategy};
+use crate::{
+    constants::SCALAR_12, reserves, test::EnvTestUtils, BlendStrategyClient, STRATEGY_NAME
+};
+
 #[test]
 fn set_validated_vault_shares() {
   let strategy = create_generic_strategy();
@@ -15,4 +33,53 @@ fn set_validated_vault_shares() {
   
   let success_result = e.as_contract(&strategy.address, || reserves::set_validated_vault_shares(&e, &strategy.address, 100));
   assert_eq!(success_result, Ok(100i128));
+}
+
+#[test]
+fn harvest_emit_event() {
+  let strategy = create_generic_strategy();
+  let e = strategy.e.clone();
+  let keeper = strategy.keeper.clone();
+
+  e.mock_all_auths();
+
+  let amount: i128 = 10 * 10_000_000;
+  mint_and_deposit_to_strategy(&strategy, &strategy.pool_admin, amount);
+
+  // Call harvest as keeper
+  let strategy_client = BlendStrategyClient::new(&e, &strategy.address);
+  strategy_client.harvest(&keeper, &None::<Bytes>);
+
+  // Fetch the last event
+  let events = e.events().all();
+  let last_event = events.last().unwrap();
+
+  let expected_topic = (String::from_str(&e, STRATEGY_NAME), symbol_short!("harvest")).into_val(&e);
+  assert_eq!(last_event.1, expected_topic);
+
+  let event_data: HarvestEvent = FromVal::from_val(&e, &last_event.2);
+  assert_eq!(event_data.from, keeper);
+  assert!(event_data.amount >= 0);
+  // it should be one since it hasnt passed time yet
+  assert!(event_data.price_per_share == SCALAR_12);
+
+  // let's pass time
+  e.jump(1000u32);
+
+  // call harvest again
+  strategy_client.harvest(&keeper, &None::<Bytes>);
+
+  // Fetch the last event
+  let events = e.events().all();
+  let last_event = events.last().unwrap();
+
+  let expected_topic = (String::from_str(&e, STRATEGY_NAME), symbol_short!("harvest")).into_val(&e);
+  assert_eq!(last_event.1, expected_topic);
+
+  let event_data: HarvestEvent = FromVal::from_val(&e, &last_event.2);
+  assert_eq!(event_data.from, keeper);
+  assert!(event_data.amount >= 0);
+  std::println!("event_data.price_per_share: {}", event_data.price_per_share);
+  // it should be greater than 1 since it has passed time
+  assert!(event_data.price_per_share > SCALAR_12);
 }
--- apps/contracts/strategies/core/src/event.rs
+++ apps/contracts/strategies/core/src/event.rs
@@ -32,6 +32,7 @@ pub fn emit_deposit(e: &Env, strategy_name: String, amount: i128, from: Address)
 pub struct HarvestEvent {
     pub amount: i128,
     pub from: Address,
+    pub price_per_share: i128,
 }
 
 /// Publishes an `HarvestEvent` to the event stream.
@@ -42,8 +43,9 @@ pub struct HarvestEvent {
 /// * `strategy_name` - The name of the strategy.
 /// * `amount` - The amount of tokens harvested.`
 /// * `from` - The address of the account/vault that harvested the tokens.
-pub fn emit_harvest(e: &Env, strategy_name: String, amount: i128, from: Address) {
-    let event = HarvestEvent { amount, from };
+/// * `price_per_share` - The price per share of the strategy or any other metric to help determine the APY. This could be multiplied by SCALAR_12 to get the price per share in 12 decimal places.
+pub fn emit_harvest(e: &Env, strategy_name: String, amount: i128, from: Address, price_per_share: i128) {
+    let event = HarvestEvent { amount, from, price_per_share };
 
     e.events()
         .publish((strategy_name, symbol_short!("harvest")), event);
