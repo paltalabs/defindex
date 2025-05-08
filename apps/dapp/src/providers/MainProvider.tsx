eb84ec19a692e20cc2729b497dd109720ab1b7f5
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-05-08 07:57:15 -0400

    💚Fix build

--- apps/dapp/src/providers/MainProvider.tsx
+++ apps/dapp/src/providers/MainProvider.tsx
@@ -2,8 +2,11 @@
 import { ReactNode } from "react"
 import MySorobanReactProvider from "./SorobanProvider"
 import { ThemeProvider } from "@/components/ui/provider"
+import useMounted from "@/hooks/useMounted"
 
 export const MainProvider = ({ children }: { children: ReactNode }) => {
+  const mounted = useMounted();
+  if (!mounted) return null;
   return (
     <MySorobanReactProvider>
       <ThemeProvider>
