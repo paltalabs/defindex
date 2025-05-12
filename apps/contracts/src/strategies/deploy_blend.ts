09fcd1445bb3f77e48c485025557e828936203ec
chopan123 <chopancatrileo@gmail.com>
2025-05-12 18:44:07 -0400

    deploying arranged and calculating first deposit amount

--- apps/contracts/src/strategies/deploy_blend.ts
+++ apps/contracts/src/strategies/deploy_blend.ts
@@ -1,4 +1,4 @@
-import { Address, nativeToScVal, xdr } from "@stellar/stellar-sdk";
+import { Address, nativeToScVal, scValToNative, xdr } from "@stellar/stellar-sdk";
 import * as fs from 'fs';
 import * as path from 'path';
 import { dirname } from 'path';
@@ -8,7 +8,9 @@ import { AddressBook } from "../utils/address_book.js";
 import {
   airdropAccount,
   deployContract,
-  installContract
+  installContract,
+  invokeContract,
+  invokeCustomContract
 } from "../utils/contract.js";
 import { config } from "../utils/env_config.js";
 
@@ -29,6 +31,7 @@ function loadBlendDeployConfig() {
 function checkBlendDeployConfigFormat(config: any) {
   const requiredEnvs = ["mainnet", "testnet"];
   const requiredStrategyFields = [
+    "name",
     "keeper",
     "asset",
     "asset_symbol",
@@ -77,6 +80,41 @@ function checkAllowedAsset(asset_symbol: string | undefined, allowedAssets: stri
   return asset_symbol !== undefined && allowedAssets.includes(asset_symbol.toUpperCase());
 }
 
+function constructBlendStrategyArgs(strategy: any): xdr.ScVal[] {
+  const initArgs = xdr.ScVal.scvVec([
+    new Address(strategy.blend_pool_address).toScVal(), // blend_pool_address: The address of the Blend pool where assets are deposited
+    new Address(BLEND_TOKEN).toScVal(), // blend_token: The address of the reward token (e.g., BLND) issued by the Blend pool
+    new Address(SOROSWAP_ROUTER).toScVal(), // soroswap_router: The address of the Soroswap AMM router for asset swaps
+    nativeToScVal(40, { type: "i128" }), // reward_threshold: The minimum reward amount that triggers reinvestment
+    new Address(strategy.keeper).toScVal() // keeper: The address of the keeper that can call the harvest function
+  ]);
+
+  return [
+    new Address(strategy.asset).toScVal(), // asset: The asset to be managed by the strategy (XLM or USDC)
+    initArgs
+  ];
+}
+
+async function calculateDepositAmount(
+  blendPoolAddress: string,
+  asset: string,
+  admin: any
+): Promise<number> {
+  const reserves = await invokeCustomContract(
+    blendPoolAddress,
+    "get_reserve",
+    [
+      new Address(asset).toScVal(),
+    ],
+    admin,
+    true
+  );
+  const reservesData = scValToNative(reserves.result.retval);
+  const b_rate = reservesData.data.b_rate;
+  
+  return b_rate * 1001 / 1000000000000 + 1;
+}
+
 export async function deployBlendStrategy(addressBook: AddressBook, asset_symbol?: string) {
   if (network == "standalone") {
     console.log("Blend Strategy can only be tested in testnet or mainnet");
@@ -103,27 +141,42 @@ export async function deployBlendStrategy(addressBook: AddressBook, asset_symbol
 
 
   for (const strategy of blendDeployConfig[network].strategies) {
+    const args = constructBlendStrategyArgs(strategy);
     
-    const initArgs = xdr.ScVal.scvVec([
-      new Address(strategy.blend_pool_address).toScVal(), // blend_pool_address: The address of the Blend pool where assets are deposited
-      new Address(BLEND_TOKEN).toScVal(), // blend_token: The address of the reward token (e.g., BLND) issued by the Blend pool
-      new Address(SOROSWAP_ROUTER).toScVal(), // soroswap_router: The address of the Soroswap AMM router for asset swaps
-      nativeToScVal(40, { type: "i128" }), // reward_threshold: The minimum reward amount that triggers reinvestment
-      new Address(strategy.keeper).toScVal() // keeper: The address of the keeper that can call the harvest function
-    ]);
-  
-    const args: xdr.ScVal[] = [
-      new Address(strategy.asset).toScVal(), // asset: The asset to be managed by the strategy (XLM or USDC)
-      initArgs
-    ];
-  
+    let contractKey = `${strategy.asset_symbol.toLowerCase()}_blend_${strategy.name}_${strategy.blend_pool_name}_strategy`
     await deployContract(
-      `${strategy.asset_symbol.toLowerCase()}_blend_${strategy.name}_${strategy.blend_pool_name}_strategy`,
+      contractKey,
       "blend_strategy",
       addressBook,
       args,
       loadedConfig.admin
     );
+
+    // We need to  get how much is the deposit amount
+    // To do that we need to get the b_rate from the reserves of the blend pool
+
+    const depositAmount = await calculateDepositAmount(
+      strategy.blend_pool_address,
+      strategy.asset,
+      loadedConfig.admin
+    );
+
+    // Deposit 2000 stroops to the strategy
+    // We need the first depositor to deposit at least 1001 stroops
+    // But since optimal deposit amount may reduce the amount of stroops,
+    // we deposit 2000 to ensure we're above the minimum threshold
+
+    await invokeContract(
+      contractKey,
+      addressBook,
+      "deposit",
+      [
+        nativeToScVal(depositAmount, { type: "i128" }),
+        new Address(loadedConfig.admin.publicKey()).toScVal(),
+      ],
+      loadedConfig.admin
+    );
+    
   }
   
 }
