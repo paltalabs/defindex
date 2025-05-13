7e5aa942bfbd033da3ee7e500a7b51d5a5699f25
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-05-13 18:16:30 -0400

    🩹Fixed routes on navbar

--- apps/dapp/src/components/NavBar/NavBar.tsx
+++ apps/dapp/src/components/NavBar/NavBar.tsx
@@ -1,17 +1,31 @@
 'use client'
 import { Box, Flex, Link, Image, HStack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import ConnectButton from '../web3/ConnectWallet'
 import './NavBar.css'
 import { navBarHeight } from '../ui/Common'
+import { useSorobanReact, WalletNetwork } from 'stellar-react'
+import { usePublicAddresses } from '@/hooks/usePublicAddresses'
+import { getNetworkName } from '@/helpers/networkName'
 
 
-const links = [
-  { name: 'Defindex Vaults', href: '/' },
-  { name: 'Portfolio', href: '/' },
-  { name: 'Defindex home', href: '/' },
-]
 function NavBar() {
+  const sorobanContext = useSorobanReact();
+  const [vaultAddress, setVaultAddress] = React.useState<string | null>(null);
+  const publicAddresses = usePublicAddresses(getNetworkName(sorobanContext.activeNetwork)).data;
+  const links = [
+    { name: 'Launch Vault', href: '/' },
+    { name: 'Paltalabs Vault', href: `/vault/${vaultAddress}` },
+  ]
+
+  useEffect(() => {
+    if (publicAddresses) {
+      const vaultAddress = sorobanContext.activeNetwork === WalletNetwork.PUBLIC ? publicAddresses['usdc_palta_vault'] : publicAddresses['usdc_blend_vault'];
+      if (vaultAddress) {
+        setVaultAddress(vaultAddress);
+      }
+    }
+  }, [publicAddresses])
   return (
     <Box>
       <Flex alignItems={'center'} className='nav-bar'>
