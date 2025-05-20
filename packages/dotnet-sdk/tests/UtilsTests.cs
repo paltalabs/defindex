dfe4a5431a0adff14814131d39b32e259281a03b
chopan123 <chopancatrileo@gmail.com>
2025-05-20 04:56:52 -0400

    populating with more realistic test values

--- packages/dotnet-sdk/tests/UtilsTests.cs
+++ packages/dotnet-sdk/tests/UtilsTests.cs
@@ -14,11 +14,11 @@ namespace DeFindex.Sdk.Tests
             // Arrange
             var poolConfig = new PoolConfig
             {
-                Oracle = "GAAZI4TCR3TY5OJHCTJC2A4QSY6CJWJH5IAJTGKIN2ER7LBNVKOCCWN7",
-                MinCollateral = 1000000000, // 1 token with 7 decimals
-                BstopRate = 500000, // 5% in 7 decimals
+                Oracle = "CD74A3C54EKUVEGUC6WNTUPOTHB624WFKXN3IYTFJGX3EHXDXHCYMXXR",
+                MinCollateral = 50000000, // 0.05 token with 7 decimals
+                BstopRate = 2000000, // 20% in 7 decimals
                 Status = 1, // Active
-                MaxPositions = 100
+                MaxPositions = 6
             };
 
             var reserveEmissionData = new ReserveEmissionData
@@ -31,17 +31,17 @@ namespace DeFindex.Sdk.Tests
 
             var reserveData = new ReserveData
             {
-                DRate = 1000000000000, // 1:1 with 12 decimals
-                BRate = 1000000000000, // 1:1 with 12 decimals
-                IrMod = 10000000, // 1 with 7 decimals
-                BSupply = 1000000000, // 1 token with 7 decimals
-                DSupply = 1000000000, // 1 token with 7 decimals
-                BackstopCredit = 0,
-                LastTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                DRate = 1008674205479, // Updated from 1007254005472
+                BRate = 1005739211313, // Updated from 1004999864177
+                IrMod = 11064275, // Updated from 8325672
+                BSupply = 1903776551256, // Updated from 12913989438990
+                DSupply = 1687391443495, // Updated from 11615633968856
+                BackstopCredit = 699805978, // Updated from 1503385393
+                LastTime = 1747730608 // Updated from 1747728348
             };
 
-            var assetReserves = (1000000000L, 1000000000L); // (1 token, 1 token) with 7 decimals
-            var blndReserves = (1000000000L, 1000000000L); // (1 token, 1 token) with 7 decimals
+            var assetReserves = (2724548742322L, 2415996815711L); // Updated from (1000000000L, 1000000000L)
+            var blndReserves = (502L, 24012L); // Updated from (1000000000L, 1000000000L)
 
             var managedFunds = new ManagedFundsResult(
                 Asset: "USDC",
@@ -78,24 +78,24 @@ namespace DeFindex.Sdk.Tests
             // Arrange
             var poolConfig = new PoolConfig
             {
-                Oracle = "GAAZI4TCR3TY5OJHCTJC2A4QSY6CJWJH5IAJTGKIN2ER7LBNVKOCCWN7",
-                MinCollateral = 1000000000,
-                BstopRate = 500000,
+                Oracle = "CD74A3C54EKUVEGUC6WNTUPOTHB624WFKXN3IYTFJGX3EHXDXHCYMXXR",
+                MinCollateral = 50000000,
+                BstopRate = 2000000,
                 Status = 1,
-                MaxPositions = 100
+                MaxPositions = 6
             };
             var reserveData = new ReserveData
             {
-                DRate = 1000000000000,
-                BRate = 1000000000000,
-                IrMod = 10000000,
-                BSupply = 1000000000,
-                DSupply = 1000000000,
-                BackstopCredit = 0,
-                LastTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                DRate = 1008674205479,
+                BRate = 1005739211313,
+                IrMod = 11064275,
+                BSupply = 1903776551256,
+                DSupply = 1687391443495,
+                BackstopCredit = 699805978,
+                LastTime = 1747730608
             };
-            var assetReserves = (1000000000L, 1000000000L);
-            var blndReserves = (1000000000L, 1000000000L);
+            var assetReserves = (2724548742322L, 2415996815711L); // Updated from (1000000000L, 1000000000L)
+            var blndReserves = (502L, 24012L); // Updated from (1000000000L, 1000000000L)
 
             // Act
             var result = Utils.calculateSupplyAPY(
@@ -118,8 +118,8 @@ namespace DeFindex.Sdk.Tests
                 Index = 1000000000,
                 LastTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
             };
-            var assetReserves = (1000000000L, 1000000000L);
-            var blndReserves = (1000000000L, 1000000000L);
+            var assetReserves = (2724548742322L, 2415996815711L); // Updated from (1000000000L, 1000000000L)
+            var blndReserves = (502L, 24012L); // Updated from (1000000000L, 1000000000L)
 
             // Act
             var result = Utils.calculateEmissionsAPR(
