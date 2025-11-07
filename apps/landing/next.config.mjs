560346420de91b60e228d757e3d262dd36e335a5
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-11-07 12:15:26 -0300

    ✨Add beans case study

--- apps/landing/next.config.mjs
+++ apps/landing/next.config.mjs
@@ -16,6 +16,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
     ],
   },
 
